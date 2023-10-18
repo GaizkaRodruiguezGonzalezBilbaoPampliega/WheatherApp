a57b67a730c2451ebd91bbc10c805a255b9312d8
Gaizka <gaizka.rodriguezgo@elorrieta-errekamari.com>
2023-10-18 08:20:57 +0200

    Agregar archivos de proyecto.

---
+++ Config.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp
+{
+    public class Config
+    {
+        //pongo aqui mi class geoapify
+        public static string GeoApifykey = "0285c5b0e08f4660904d080a7370cb8c";
+    }
+}
